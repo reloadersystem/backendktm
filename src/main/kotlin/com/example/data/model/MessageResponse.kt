500590c214d89863f3c9f458d7c16f3263820046
suarezfc82@gmail.com <suarezfc82@gmail.com>
2024-06-08 11:39:52 -0500

    webservices_complete

---
+++ src/main/kotlin/com/example/data/model/MessageResponse.kt
@@ -0,0 +1,7 @@
+package com.example.data.model
+
+import kotlinx.serialization.Serializable
+
+
+@Serializable
+data class MessageResponse(val message: String)
--- src/main/kotlin/com/example/route/ExpenseRoute.kt
+++ src/main/kotlin/com/example/route/ExpenseRoute.kt
@@ -1,8 +1,11 @@
 package com.example.route
 
+import com.example.data.model.MessageResponse
+import com.example.data.model.Expense
 import com.example.data.model.expenses
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 
@@ -15,4 +18,45 @@ fun Route.expensesRouting() {
             call.respond(status = HttpStatusCode.OK, expenses)
         }
     }
+
+    get("/expenses/{id}") {
+
+        val id = call.parameters["id"]?.toLongOrNull()
+        if(id == null || id !in 0 until expenses.size) {
+            call.respond(HttpStatusCode.NotFound, MessageResponse("Expense not found"))
+            return@get
+        }
+        call.respond(HttpStatusCode.OK, expenses[id.toInt()])
+    }
+
+    post("expenses"){
+        val expense = call.receive<Expense>()
+        expenses.add(expense)
+        call.respond(HttpStatusCode.OK, MessageResponse(" Expense add Successfully"))
+    }
+
+    put("expenses/{id}"){
+        val id = call.parameters["id"]?.toLongOrNull()
+        val expense = call.receive<Expense>()
+        if(id == null || id !in 0 until expenses.size) {
+            call.respond(HttpStatusCode.NotFound, MessageResponse("Expense not found"))
+            return@put
+        }
+
+        val index = expenses.indexOfFirst { it.id == id }
+        expenses[index] = expense.copy(id = id)
+        call.respond(HttpStatusCode.OK, MessageResponse("Expense update Successfully"))
+
+    }
+
+    delete("expenses/{id}"){
+        val id = call.parameters["id"]?.toLongOrNull()
+        if(id == null || id !in 0 until expenses.size) {
+            call.respond(HttpStatusCode.NotFound, MessageResponse("Expense not found"))
+            return@delete
+        }
+        expenses.removeIf { it.id == id }
+        call.respond(HttpStatusCode.OK, MessageResponse("Expense Remove Successfully"))
+    }
+
 }
